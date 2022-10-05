8fac22b21a7393a18dda387aa6cac16e3d27514f
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-10-05 15:43:04 +0900

    setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <div>asdf</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<div>Main</div>} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
