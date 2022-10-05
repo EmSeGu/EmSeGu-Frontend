d680a51f247e633d8bec52a969054d909ffb56ad
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-10-05 16:19:38 +0900

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginComponent from "./components/Login";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<div>Main</div>} />
+        <Route path="/login" element={<LoginComponent />} />
         <Route path="*" element={<div>Not Found</div>} />
       </Routes>
     </BrowserRouter>
