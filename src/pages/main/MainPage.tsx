e6fd1af0c80741b484528ae847a06e823ba3ba87
kimhyerin0909 <khyerin0909@gmail.com>
2022-10-06 11:22:39 +0900

    stock list

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Possession from "../../components/home/possession";
 import CardList from "../../components/home/possession/card";
+import { StockInfo } from "../../components/home/Stock";
 
 const MainPage = () => {
   return (
     <div>
       <Possession />
       <CardList />
+      <StockInfo />
     </div>
   );
 };
