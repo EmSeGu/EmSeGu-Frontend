e2ee0b68749f9b5ee471d72fafd76a7d23dd29fe
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-10-06 15:41:24 +0900

    news

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+import NewsList from "../../components/home/news/NewsList";
 import Possession from "../../components/home/possession";
 import CardList from "../../components/home/possession/card";
 
 const MainPage = () => {
   return (
-    <div>
-      <Possession />
-      <CardList />
-    </div>
+    <>
+      <div>
+        <Possession />
+        <CardList />
+      </div>
+      <div style={{ marginLeft: "20px" }}>
+        <NewsList />
+      </div>
+    </>
   );
 };
 
